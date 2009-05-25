eab78d39866f356e10049fb5fa420af2db14d04c
Blake Smith <blakesmith0@gmail.com>
2009-05-24 21:00:55 -0500

    Preliminary square class.

--- src/square.rb
+++ src/square.rb
@@ -1,2 +1,18 @@
 class Square
+  attr_accessor :left, :top, :right, :bottom, :top_left, :top_right, :bot_right, :bot_left
+  attr_accessor :coordinates, :piece, :color
+
+  def initialize(color=nil, coordinates=nil)
+    self.color = color
+    self.coordinates = coordinates
+  end
+
+  def is_empty
+    if @piece.nil?
+      true
+    else
+      false
+    end
+  end
+
 end
