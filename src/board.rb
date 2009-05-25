e983ca05e8bfcfd3fda4f816f19b09e40c5f99fc
Blake Smith <blakesmith0@gmail.com>
2009-05-25 02:32:41 -0500

    Board creation with square coordinates/colors.

--- src/board.rb
+++ src/board.rb
@@ -1,2 +1,24 @@
+require 'square'
+
 class Board
+  attr_accessor :squares
+
+  def initialize
+    @squares = []
+    create_squares
+  end
+
+  def create_squares
+    for row in 1..8
+      for column in 1..8
+        if row.even? and column.even? or row.odd? and column.odd?
+          color = 'black'
+        else
+          color = 'white'
+        end
+        @squares.push Square.new color, [row, column]
+      end
+    end
+  end
+
 end
--- src/square.rb
+++ src/square.rb
@@ -3,8 +3,8 @@ class Square
   attr_accessor :coordinates, :piece, :color
 
   def initialize(color=nil, coordinates=nil)
-    self.color = color
-    self.coordinates = coordinates
+    @color = color
+    @coordinates = coordinates
   end
 
   def is_empty
@@ -12,6 +12,13 @@ class Square
       true
     else
       false
+    end
+  end
+
+  def inspect
+    cords = { 1 => 'a', 2 => 'b', 3 => 'c', 4 => 'd', 5 => 'e', 6 => 'f', 7 => 'g', 8 => 'h' }
+    if not @color.nil? and not @coordinates.nil?
+      cords[@coordinates[0]] + @coordinates[1].to_s + ': ' + @color
     end
   end
 
