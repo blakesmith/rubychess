44d415dd214e8eec2eb181d383e75a98a42caf8e
Blake Smith <blakesmith0@gmail.com>
2009-05-26 23:14:35 -0500

    Pawn diagonal capture.

--- src/pieces/pawn.rb
+++ src/pieces/pawn.rb
@@ -3,6 +3,7 @@ require 'piece'
 class Pawn < Piece
 
   def valid_moves
+    #En passant handled by the game class each move? Not sure yet.
     moves = []
     if @move_count == 0 #First move of the game.
       move_two_squares = @square.top.top
@@ -13,6 +14,13 @@ class Pawn < Piece
     move_one_square = @square.top #Normal 1 piece move
     if move_one_square.empty?
       moves.push move_one_square
+    end
+    for square in [@square.top_right, @square.top_left] #Diagonal capture
+      if not square.empty?
+        if square.piece.color != @color
+          moves.push square
+        end
+      end
     end
     moves
   end
