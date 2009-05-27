39fa3d320ef6509ccb881d421ab8d4b0b5c008cf
Blake Smith <blakesmith0@gmail.com>
2009-05-26 23:24:47 -0500

    Account for if the pawn is black or white (Which direction to move/capture)

--- src/pieces/pawn.rb
+++ src/pieces/pawn.rb
@@ -5,17 +5,25 @@ class Pawn < Piece
   def valid_moves
     #En passant handled by the game class each move? Not sure yet.
     moves = []
-    if @move_count == 0 #First move of the game.
+    if @color == 'white'
       move_two_squares = @square.top.top
-      if move_two_squares.empty? and @square.top.empty? 
+      move_one_square = @square.top #Normal 1 piece move
+      capture_diagonal = [@square.top_right, @square.top_left]
+    end
+    if @color == 'black' 
+      move_two_squares = @square.bottom.bottom
+      move_one_square = @square.bottom #Normal 1 piece move
+      capture_diagonal = [@square.bot_right, @square.bot_left]
+    end
+    if @move_count == 0 #First move of the game.
+      if move_two_squares.empty? and move_one_square.empty? 
         moves.push move_two_squares
       end
     end
-    move_one_square = @square.top #Normal 1 piece move
     if move_one_square.empty?
       moves.push move_one_square
     end
-    for square in [@square.top_right, @square.top_left] #Diagonal capture
+    capture_diagonal.each do |square| #Diagonal capture
       if not square.empty?
         if square.piece.color != @color
           moves.push square
