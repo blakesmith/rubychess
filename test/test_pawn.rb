547949669dbb6544d34757850572e32b48c7f7fd
Blake Smith <blakesmith0@gmail.com>
2009-06-09 14:42:48 -0500

    Add some primitive pawn tests.

---
+++ test/test_pawn.rb
@@ -0,0 +1,25 @@
+$LOAD_PATH << '../lib'
+$LOAD_PATH << '../lib/pieces'
+
+require 'test/unit'
+require 'board'
+
+class TestPawn < Test::Unit::TestCase
+
+  def setup
+    @board = Board.new
+  end
+
+  #Tests whether white and black can both move 1 squares in the opening position of the game.
+  def test_pawn_initial_single_move
+    assert_nothing_raised(RuntimeError) do 
+      @board.do_move(@board.find_square('d2'), @board.find_square('d3'))
+      @board.do_move(@board.find_square('d7'), @board.find_square('d6'))
+    end
+  end
+
+  def test_square_count
+    assert_equal(64, @board.squares.length)
+  end
+
+end
